8e81d363c08cbea6df7f7a167cb272e469e85cc5
Kai Takami <kaitakami@protonmail.com>
2022-10-29 22:47:37 +0900

    Install internationalized routering using i18n

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  i18n: {
+    locales: ["en-US", "ja-JP"],
+    defaultLocale: "en-US",
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;

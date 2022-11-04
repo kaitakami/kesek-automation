efb0d2f9cce7ce013d9d480b7178ebf31b517b15
Kai Takami <kaitakami@protonmail.com>
2022-11-04 22:44:51 +0900

    Make hypgraph API fetch

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,37 @@
 import Link from "next/link";
 import Layout from "components/Layout";
+import { request } from "graphql-request";
 import styles from "styles/Home.module.css";
 
 export default function Home(props) {
-  console.log(props.locale);
+  // console.log(props.locale);
+
+  const endpoint =
+    "https://api-us-west-2.hygraph.com/v2/cla11lozg08tu01up9hraaoxa/master";
+  const query = `{
+      services {
+        id
+        title
+        briefDescription
+        description {
+          html
+        }
+        image {
+          url
+        }
+      }
+    }`;
+  const fetchAPI = async () => {
+    const data = await request(endpoint, query);
+    console.log(data);
+  };
+  fetchAPI();
   return (
     <Layout title="Home">
       <div>
         <main>
           <Link href="/" locale="ja">
-            To /ja/
+            To ja
           </Link>
         </main>
       </div>

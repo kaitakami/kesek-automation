e604f24b643744a7699278d0c244b2bb55046d67
Kai Takami <kaitakami@protonmail.com>
2022-11-05 23:26:42 +0900

    Create local translation files

---
+++ locale/en.js
@@ -0,0 +1,13 @@
+const en = {
+  nav: [
+    { text: "Home", slug: "/" },
+    { text: "About", slug: "/about" },
+    { text: "Services", slug: "/services" },
+    { text: "Careers", slug: "/careers" },
+    { text: "Contact", slug: "/contact" },
+  ],
+  kesekDescription: "Kesek Automation is an industrial automation company. We specialize in vision technologies and servo controlled system design. Offering solutions, presales and post sales dedicated to our customer´s success.",
+
+};
+
+export default en;
---
+++ locale/ja.js
@@ -0,0 +1,12 @@
+const ja = {
+  nav: [
+    { text: "ホーム", slug: "/" },
+    { text: "企業情報", slug: "/about" },
+    { text: "サービス", slug: "/services" },
+    { text: "採用情報", slug: "/careers" },
+    { text: "お問合せ", slug: "/contact" },
+  ],
+  kesekDescription: "Kesek Automationは産業オートメーション企業です。視覚技術やサーボ機構設計を専門にしており、お客様の成功のためにプレセールス／ポストセールスに渡り、ソリューションを提供しています。",
+};
+
+export default ja;
